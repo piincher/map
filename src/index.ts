45ccbf2bd51d7d2c8fd1cc946e57e98356f42039
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-18 20:08:59 +0800

    show the map on the screen

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
-const user = new User();
-console.log(user);
+// import { User } from './User';
+// import { Company } from './Company';
 
-const company = new Company();
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+	zoom: 1,
+	center: {
+		lat: 0,
+		lng: 0
+	}
+});
