badb3938b206991ffacc0223931970fd004fc2d8
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-18 16:59:51 +0800

    adding google map support

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -123153,7 +123153,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53374" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49507" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
