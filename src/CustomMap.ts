ab950e151d1e812a228ea91375facb442ff4ffad
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-20 11:19:30 +0800

    add user marker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,5 @@
+import { User } from './User';
+import { Company } from './Company';
 export class CustomMap {
 	private googleMap: google.maps.Map;
 
@@ -10,4 +12,16 @@ export class CustomMap {
 			}
 		});
 	}
+
+	addUserMarker(user: User): void {
+		new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: user.location.lat,
+				lng: user.location.lng
+			}
+		});
+	}
+
+	addCompanyMarker(company: Company): void {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
 import { CustomMap } from './CustomMap';
-new CustomMap('map');
+const user = new User();
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user);
