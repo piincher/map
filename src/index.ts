b224d16df06de7d774a0350384c493118f05c96a
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-20 11:39:45 +0800

    add company  marker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './User';
-// import { Company } from './Company';
+import { Company } from './Company';
 import { CustomMap } from './CustomMap';
+
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap('map');
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
