313196342fb63a1eae88a9a4c977c46408438e6d
Ibrahim Kouma <ikouma269@gmail.com>
2021-03-21 14:27:09 +0800

    type check

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -123111,7 +123111,7 @@ function () {
 
 exports.Company = Company;
 },{"faker":"node_modules/faker/index.js"}],"src/CustomMap.ts":[function(require,module,exports) {
-"use strict";
+"use strict"; //Instruction to every other class
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -123131,22 +123131,12 @@ function () {
     });
   }
 
-  CustomMap.prototype.addUserMarker = function (user) {
+  CustomMap.prototype.addMarker = function (mappable) {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
-      }
-    });
-  };
-
-  CustomMap.prototype.addCompanyMarker = function (company) {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     });
   };
@@ -123171,8 +123161,8 @@ var CustomMap_1 = require("./CustomMap");
 var user = new User_1.User();
 var company = new Company_1.Company();
 var customMap = new CustomMap_1.CustomMap('map');
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
 },{"./User":"src/User.ts","./Company":"src/Company.ts","./CustomMap":"src/CustomMap.ts"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -123201,7 +123191,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49181" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49884" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,14 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
-
 //Instruction to every other class
 
 //on how they can be an argument to 'addMarker
 
 interface Mappable {
 	location: {
-		lat: Number;
-		lng: Number;
+		lat: number;
+		lng: number;
 	};
 }
 export class CustomMap {
@@ -33,14 +30,4 @@ export class CustomMap {
 			}
 		});
 	}
-
-	// addCompanyMarker(company: Company): void {
-	// 	new google.maps.Marker({
-	// 		map: this.googleMap,
-	// 		position: {
-	// 			lat: company.location.lat,
-	// 			lng: company.location.lng
-	// 		}
-	// 	});
-	// }
 }
