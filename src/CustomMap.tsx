26032c5a475b9372950221b49faaa93e35a2e19f
Ibrahim <ikouma269@gmail.com>
2022-08-12 23:22:19 +0800

    add marker

---
+++ src/CustomMap.tsx
@@ -0,0 +1,38 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: { lat: 0, lng: 0 },
+      }
+    );
+  }
+  addMArker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "helooo",
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
-const company = new Company();
+// const company = new Company();
 
-console.log(user);
-console.log(company);
+const map = new CustomMap("map ");
+map.addMArker(user);
+
+// console.log(user);
+// console.log(company);
